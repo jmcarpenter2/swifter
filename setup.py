0d1196f2c393d7fd424932cdaf07a9df1ef29223
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-08-13 13:51:00 -0700

    change init

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 setup(
   name = 'swifter',
   packages = ['swifter'], # this must be the same as the name above
-  version = '0.153',
+  version = '0.201',
   description = 'A package which efficiently applies any function to a pandas dataframe or series in the fastest available manner',
   author = 'Jason Carpenter',
   author_email = 'jmcarpenter2@dons.usfca.edu',
   url = 'https://github.com/jmcarpenter2/swifter', # use the URL to the github repo
-  download_url = 'https://github.com/jmcarpenter2/swifter/archive/0.152.tar.gz',
+  download_url = 'https://github.com/jmcarpenter2/swifter/archive/0.200.tar.gz',
   keywords = ['pandas', 'apply', 'function', 'parallelize', 'vectorize'],
   install_requires=[
         'pandas',
         'psutil',
-        'dask'
+        'dask[complete]'
   ],
   classifiers = [],
 )
