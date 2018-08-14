c268c4d7554ab537e395b93299e0014f37b6c925
jmcarpenter2 <jmcarpenter2@dons.usfca.edu>
2018-08-14 07:36:26 -0700

    Bug fix for row-wise applies

--- swifter/swifter.py
+++ swifter/swifter.py
@@ -4,7 +4,6 @@ from dask import dataframe as dd
 import timeit
 import warnings
 
-
 @pd.api.extensions.register_series_accessor("swifter")
 class SeriesAccessor():
     def __init__(self, pandas_series, npartitions=None, dask_threshold=1):
@@ -59,7 +58,6 @@ class SeriesAccessor():
                 kwds.pop('meta')
                 return self._obj.apply(func, convert_dtype=convert_dtype, args=args, **kwds)
 
-
 @pd.api.extensions.register_dataframe_accessor("swifter")
 class DataFrameAccessor():
     def __init__(self, pandas_dataframe, npartitions=None, dask_threshold=1):
@@ -71,13 +69,14 @@ class DataFrameAccessor():
             self._obj.npartitions = npartitions
         self._obj.dask_threshold = dask_threshold
 
-    def _wrapped_apply(self, func, axis=1, broadcast=None, raw=False, reduce=None, result_type=None, args=(), **kwds):
+
+    def _wrapped_apply(self, func, axis=0, broadcast=None, raw=False, reduce=None, result_type=None, args=(), **kwds):
         def wrapped():
-            pd.concat([self._obj.loc[:1000, c].apply(func, convert_dtype=True, args=args, **kwds)
-                       for c in self._obj.columns], axis=axis)
+            self._obj.iloc[:1000, :].apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
+                                       result_type=result_type, args=args, **kwds)
         return wrapped
 
-    def _dask_apply(self, func, axis=1, *args, **kwds):
+    def _dask_apply(self, func, axis=0, *args, **kwds):
         samp = self._obj.iloc[:self._obj.npartitions*2]
         tmp = kwds.pop('meta')
         meta = {c: tmp[c].dtype for c in tmp.columns}
@@ -92,26 +91,25 @@ class DataFrameAccessor():
             return pd.concat([self._obj[c].swifter.apply(func, *args, **kwds)
                               for c in self._obj.columns], axis=1)
 
+
     def apply(self, func, axis=1, broadcast=None, raw=False, reduce=None, result_type=None, args=(), **kwds):
-        samp = self._obj.iloc[:1000]
+        samp = self._obj.iloc[:1000, :]
 
         try:  # try to vectorize
-            tmp_df = pd.concat([pd.Series(func(samp[c], *args, **kwds), name=c)
-                                for c in samp.columns], axis=axis)
+            tmp_df = func(samp, *args, **kwds)
             assert tmp_df.shape == samp.shape
-            return pd.concat([pd.Series(func(self._obj[c], *args, **kwds), name=c)
-                              for c in self._obj.columns], axis=1)
+            return func(self._obj, *args, **kwds)
         except (AssertionError, AttributeError, ValueError) as e:  # if can't vectorize, estimate time to pandas apply
             wrapped = self._wrapped_apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
-                                          result_type=result_type, args=args, **kwds)
+                                       result_type=result_type, args=args, **kwds)
             n_repeats = 3
             timed = timeit.timeit(wrapped, number=n_repeats)
             samp_proc_est = timed / n_repeats
             est_apply_duration = samp_proc_est / 1000 * self._obj.shape[0]
 
             # Get meta information for dask, and check if output is str
-            kwds['meta'] = pd.concat([self._obj.loc[:1000, c].apply(func, convert_dtype=True, args=args, **kwds)
-                                      for c in self._obj.columns], axis=1)
+            kwds['meta'] = self._obj.iloc[:1000, :].apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
+                                       result_type=result_type, args=args, **kwds)
             str_object = object in kwds['meta'].dtypes.values
 
             # if pandas apply takes too long and output is not str, use dask
@@ -119,5 +117,5 @@ class DataFrameAccessor():
                 return self._dask_apply(func, axis=axis, *args, **kwds)
             else:  # use pandas
                 kwds.pop('meta')
-                return pd.concat([self._obj.loc[:1000, c].apply(func, convert_dtype=True, args=args, **kwds)
-                                  for c in self._obj.columns], axis=1)
+                return self._obj.apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
+                                       result_type=result_type, args=args, **kwds)
