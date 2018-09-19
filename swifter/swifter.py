869df80b4d242e769762d1b450593c61f9a723be
jmcarpenter2 <jcarpenter@manifold.ai>
2018-09-19 14:52:05 -0700

    Added TQDM support, removed groupby_apply, and tweaked some under-the-hood _dask_apply functionality

--- swifter/swifter.py
+++ swifter/swifter.py
@@ -3,6 +3,8 @@ from psutil import cpu_count
 from dask import dataframe as dd
 import timeit
 import warnings
+from tqdm import tqdm
+from tqdm_dask_progressbar import TQDMDaskProgressBar
 
 
 @pd.api.extensions.register_series_accessor("swifter")
@@ -21,22 +23,24 @@ class SeriesAccessor:
                 self._obj.iloc[:1000].apply(func, convert_dtype=convert_dtype, args=args, **kwds)
         return wrapped
 
-    def _dask_apply(self, func, axis=0, *args, **kwds):
+    def _dask_apply(self, func, convert_dtype, *args, **kwds):
         samp = self._obj.iloc[:self._obj.npartitions*2]
-        meta = kwds.pop('meta')
+        meta = samp.apply(func, convert_dtype=convert_dtype, args=args, **kwds)
         try:
             tmp_df = dd.from_pandas(samp, npartitions=self._obj.npartitions). \
                 map_partitions(func, *args, meta=meta, **kwds).compute(scheduler='processes')
             assert tmp_df.shape == meta.shape
-            return dd.from_pandas(self._obj, npartitions=self._obj.npartitions). \
-                map_partitions(func, *args, meta=meta, **kwds).compute(scheduler='processes')
+            with TQDMDaskProgressBar(desc='Dask Apply'):
+                return dd.from_pandas(self._obj, npartitions=self._obj.npartitions). \
+                    map_partitions(func, *args, meta=meta, **kwds).compute(scheduler='processes')
         except (AssertionError, AttributeError, ValueError, TypeError) as e:
-            return dd.from_pandas(self._obj, npartitions=self._obj.npartitions). \
-                map(lambda x: func(x, *args, **kwds), meta=meta).compute(scheduler='processes')
+            with TQDMDaskProgressBar(desc='Dask Apply'):
+                return dd.from_pandas(self._obj, npartitions=self._obj.npartitions). \
+                    apply(lambda x: func(x, *args, **kwds), meta=meta).compute(scheduler='processes')
 
     def apply(self, func, convert_dtype=True, args=(), **kwds):
         samp = self._obj.iloc[:self._obj.npartitions*2]
-        str_object = samp.dtype == 'object' # check if input is string
+        str_object = samp.dtype == 'object'  # check if input is string
 
         if 'axis' in kwds.keys():
             kwds.pop('axis')
@@ -53,15 +57,12 @@ class SeriesAccessor:
             samp_proc_est = timed / n_repeats
             est_apply_duration = samp_proc_est / 1000 * self._obj.shape[0]
 
-            # Get meta information for dask
-            kwds['meta'] = self._obj.iloc[:2].apply(func, convert_dtype=convert_dtype, args=args, **kwds)
-
             # if pandas apply takes too long and input is not str, use dask
             if (est_apply_duration > self._obj.dask_threshold) and (not str_object):
-                return self._dask_apply(func, *args, **kwds)
+                return self._dask_apply(func, convert_dtype, *args, **kwds)
             else:  # use pandas
-                kwds.pop('meta')
-                return self._obj.apply(func, convert_dtype=convert_dtype, args=args, **kwds)
+                tqdm.pandas(desc='Pandas Apply')
+                return self._obj.progress_apply(func, convert_dtype=convert_dtype, args=args, **kwds)
 
 
 @pd.api.extensions.register_dataframe_accessor("swifter")
@@ -78,30 +79,28 @@ class DataFrameAccessor:
     def _wrapped_apply(self, func, axis=0, broadcast=None, raw=False, reduce=None, result_type=None, args=(), **kwds):
         def wrapped():
             self._obj.iloc[:1000, :].apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
-                                       result_type=result_type, args=args, **kwds)
+                                           result_type=result_type, args=args, **kwds)
         return wrapped
 
-    def _dask_apply(self, func, axis=0, *args, **kwds):
+    def _dask_apply(self, func, axis=0, broadcast=None, raw=False, reduce=None, result_type=None, *args, **kwds):
         samp = self._obj.iloc[:self._obj.npartitions*2, :]
-        tmp = kwds.pop('meta')
-        if type(tmp) == pd.Series:
-            meta = tmp
-        else:
-            meta = {c: tmp[c].dtype for c in tmp.columns}
+        meta = samp.apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
+                          result_type=result_type, args=args, **kwds)
         try:
             tmp_df = dd.from_pandas(samp, npartitions=self._obj.npartitions).\
                 apply(func, *args, axis=axis, meta=meta, **kwds).compute(scheduler='processes')
             assert tmp_df.shape == meta.shape
-            return dd.from_pandas(self._obj, npartitions=self._obj.npartitions).\
-                apply(func, *args, axis=axis, meta=meta, **kwds).compute(scheduler='processes')
+            with TQDMDaskProgressBar(desc='Dask Apply'):
+                return dd.from_pandas(self._obj, npartitions=self._obj.npartitions).\
+                    apply(func, *args, axis=axis, meta=meta, **kwds).compute(scheduler='processes')
         except (AssertionError, AttributeError, ValueError, TypeError) as e:
-            warnings.warn('Dask applymap not working correctly. Concatenating swiftapplies instead.')
-            return pd.concat([self._obj[c].swifter.apply(func, *args, **kwds)
-                              for c in self._obj.columns], axis=1)
+            tqdm.pandas(desc='Pandas Apply')
+            return self._obj.progress_apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
+                                            result_type=result_type, args=args, **kwds)
 
     def apply(self, func, axis=1, broadcast=None, raw=False, reduce=None, result_type=None, args=(), **kwds):
         samp = self._obj.iloc[:self._obj.npartitions*2, :]
-        str_object = 'object' in samp.dtypes.values # check if input is string
+        str_object = 'object' in samp.dtypes.values  # check if input is string
 
         try:  # try to vectorize
             if 'axis' in kwds.keys():
@@ -111,51 +110,50 @@ class DataFrameAccessor:
             return func(self._obj, *args, **kwds)
         except (AssertionError, AttributeError, ValueError, TypeError) as e:  # if can't vectorize, estimate time to pandas apply
             wrapped = self._wrapped_apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
-                                       result_type=result_type, args=args, **kwds)
+                                          result_type=result_type, args=args, **kwds)
             n_repeats = 3
             timed = timeit.timeit(wrapped, number=n_repeats)
             samp_proc_est = timed / n_repeats
             est_apply_duration = samp_proc_est / 1000 * self._obj.shape[0]
 
-            # Get meta information for dask
-            kwds['meta'] = self._obj.iloc[:self._obj.npartitions*2, :].apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
-                                       result_type=result_type, args=args, **kwds)
-
             # if pandas apply takes too long and input is not str, use dask
             if (est_apply_duration > self._obj.dask_threshold) and (not str_object):
-                return self._dask_apply(func, axis=axis, *args, **kwds)
+                return self._dask_apply(func, axis=axis, broadcast=None, raw=False, reduce=None, result_type=None, *args, **kwds)
             else:  # use pandas
-                kwds.pop('meta')
-                return self._obj.apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
-                                       result_type=result_type, args=args, **kwds)
+                tqdm.pandas(desc='Pandas Apply')
+                return self._obj.progress_apply(func, axis=axis, broadcast=broadcast, raw=raw, reduce=reduce,
+                                                result_type=result_type, args=args, **kwds)
 
-    def _wrapped_groupby_apply(self, groupby_col, func, *args, **kwds):
-        def wrapped():
-            self._obj.iloc[:1000, :].groupby(groupby_col).apply(func, *args, **kwds)
-        return wrapped
+####################################################################################################################
 
-    def _dask_groupby_apply(self, groupby_col, func, *args, **kwds):
-        tmp = kwds.pop('meta')
-        meta = {c: tmp[c].dtype for c in tmp.columns if c is not groupby_col}
-        try:
-            return dd.from_pandas(self._obj, npartitions=self._obj.npartitions).set_index(groupby_col).\
-                groupby(groupby_col).apply(func, *args, meta=meta, **kwds).compute(scheduler='processes')
-        except (AssertionError, AttributeError, ValueError, TypeError) as e:
-            print(e)
-
-    def groupby_apply(self, groupby_col, func, *args, **kwds):
-        wrapped = self._wrapped_groupby_apply(groupby_col, func, *args, **kwds)
-        n_repeats = 3
-        timed = timeit.timeit(wrapped, number=n_repeats)
-        samp_proc_est = timed / n_repeats
-        est_apply_duration = samp_proc_est / 1000 * self._obj.shape[0]
-
-        # Get meta information for dask
-        kwds['meta'] = self._obj.iloc[:self._obj.npartitions*2, :].groupby(groupby_col).apply(func, *args, **kwds)
-
-        # if pandas apply takes too long, use dask
-        if (est_apply_duration) > self._obj.dask_threshold:
-            return self._dask_groupby_apply(groupby_col, func, *args, **kwds)
-        else: # use pandas
-            kwds.pop('meta')
-            return self._obj.groupby(groupby_col).apply(func, *args, **kwds)
+    # EXCLUDING THIS CODE BECAUSE DASK GROUPBY-APPLY IS SLOWER THAN PANDAS
+    # def _wrapped_groupby_apply(self, groupby_col, func, *args, **kwds):
+    #     def wrapped():
+    #         self._obj.iloc[:1000, :].groupby(groupby_col).apply(func, *args, **kwds)
+    #     return wrapped
+    #
+    # def _dask_groupby_apply(self, groupby_col, func, *args, **kwds):
+    #     tmp = kwds.pop('meta')
+    #     meta = {c: tmp[c].dtype for c in tmp.columns if c is not groupby_col}
+    #     try:
+    #         return dd.from_pandas(self._obj, npartitions=self._obj.npartitions).\
+    #             groupby(groupby_col).apply(func, *args, meta=meta, **kwds).compute(scheduler='processes')
+    #     except (AssertionError, AttributeError, ValueError, TypeError) as e:
+    #         print(e)
+    #
+    # def groupby_apply(self, groupby_col, func, *args, **kwds):
+    #     wrapped = self._wrapped_groupby_apply(groupby_col, func, *args, **kwds)
+    #     n_repeats = 3
+    #     timed = timeit.timeit(wrapped, number=n_repeats)
+    #     samp_proc_est = timed / n_repeats
+    #     est_apply_duration = samp_proc_est / 1000 * self._obj.shape[0]
+    #
+    #     # Get meta information for dask
+    #     kwds['meta'] = self._obj.iloc[:self._obj.npartitions*2, :].groupby(groupby_col).apply(func, *args, **kwds)
+    #
+    #     # if pandas apply takes too long, use dask
+    #     if (est_apply_duration) > self._obj.dask_threshold:
+    #         return self._dask_groupby_apply(groupby_col, func, *args, **kwds)
+    #     else: # use pandas
+    #         kwds.pop('meta')
+    #         return self._obj.groupby(groupby_col).apply(func, *args, **kwds)
---
+++ swifter/tqdm_dask_progressbar.py
@@ -0,0 +1,31 @@
+from dask.callbacks import Callback
+from tqdm import tqdm_notebook
+
+
+class TQDMDaskProgressBar(Callback):
+    """
+    A tqdm progress bar for dask.
+
+    Usage:
+        ```
+        with TQDMDaskProgressBar():
+            da.compute()
+        ```
+    Author : wassname
+    Source : https://gist.github.com/wassname/1837d0365247430e02abda41f0e7f184
+    See: http://dask.pydata.org/en/latest/diagnostics-local.html?highlight=progress
+    """
+
+    def __init__(self, start=None, start_state=None, pretask=None, posttask=None, finish=None, **kwargs):
+        super().__init__(start=start, start_state=start_state, pretask=pretask, posttask=posttask, finish=finish)
+        self.tqdm_args = kwargs
+
+    def _start_state(self, dsk, state):
+        self._tqdm = tqdm_notebook(total=sum(len(state[k]) for k in ['ready', 'waiting', 'running', 'finished']),
+                                   **self.tqdm_args)
+
+    def _posttask(self, key, result, dsk, state, worker_id):
+        self._tqdm.update(1)
+
+    def _finish(self, dsk, state, errored):
+        self._tqdm.close()
