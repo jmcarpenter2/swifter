16b23392831944c696fcd245a55afa197abde4cf
jmcarpenter2 <jcarpenter@manifold.ai>
2018-11-21 13:38:02 -0800

    Option overrides and code styling with black

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='swifter',
     packages=['swifter'],  # this must be the same as the name above
-    version='0.260',
+    version='0.261',
     description='A package which efficiently applies any function to a pandas dataframe or series in the fastest available manner',
     author='Jason Carpenter',
-    author_email='jmcarpenter2@dons.usfca.edu',
+    author_email='jcarpenter@manifold.ai',
     url='https://github.com/jmcarpenter2/swifter',  # use the URL to the github repo
-    download_url='https://github.com/jmcarpenter2/swifter/archive/0.260.tar.gz',
+    download_url='https://github.com/jmcarpenter2/swifter/archive/0.261.tar.gz',
     keywords=['pandas', 'dask', 'apply', 'function', 'parallelize', 'vectorize'],
     install_requires=[
         'pandas>=0.23.0',
