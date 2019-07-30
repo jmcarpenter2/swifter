b0f5319d26b6706422ec084c4860479d4684c116
Nikolay Korolev <korolevns98@gmail.com>
2019-07-30 13:16:13 +0200

    Change import from tqdm.autonotebook to import tqdm.auto. Fix #66

--- swifter/swifter.py
+++ swifter/swifter.py
@@ -6,7 +6,7 @@ from math import ceil
 from psutil import cpu_count
 from dask import dataframe as dd
 
-from tqdm.autonotebook import tqdm
+from tqdm.auto import tqdm
 from .tqdm_dask_progressbar import TQDMDaskProgressBar
 
 from numba.errors import TypingError
