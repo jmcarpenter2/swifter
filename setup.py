3253f48add982d20600770e56a3364afc6a8b303
jmcarpenter2 <jcarpenter@manifold.ai>
2019-09-12 12:53:59 -0700

    Suppress print messages during sample & test applies

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages, setup, Extension
 setup(
     name="swifter",
     packages=["swifter"],  # this must be the same as the name above
-    version="0.294",
+    version="0.295",
     description="A package which efficiently applies any function to a pandas dataframe or series in the fastest available manner",
     author="Jason Carpenter",
     author_email="jcarpenter@manifold.ai",
     url="https://github.com/jmcarpenter2/swifter",  # use the URL to the github repo
-    download_url="https://github.com/jmcarpenter2/swifter/archive/0.294.tar.gz",
+    download_url="https://github.com/jmcarpenter2/swifter/archive/0.295.tar.gz",
     keywords=["pandas", "dask", "apply", "function", "parallelize", "vectorize"],
     install_requires=["pandas>=0.23.0", "psutil", "dask[complete]>=0.19.0", "tqdm>=4.33.0", "ipywidgets>=7.0.0", "parso>0.4.0", "numba"],
     classifiers=[],
