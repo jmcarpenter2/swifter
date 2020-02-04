b21a5deadb1c58a77fe1814435dc5398c74021e7
jmcarpenter2 <jcarpenter@manifold.ai>
2020-02-04 11:03:28 -0800

    Resize tests

--- swifter/swifter_tests.py
+++ swifter/swifter_tests.py
@@ -220,7 +220,7 @@ class TestSwifter(unittest.TestCase):
         self.assertLess(swifter_time, pd_time)
 
     def test_nonvectorized_math_apply_on_large_series(self):
-        df = pd.DataFrame({"x": np.random.normal(size=1_000_000)})
+        df = pd.DataFrame({"x": np.random.normal(size=5_000_000)})
         series = df["x"]
 
         start_pd = time.time()
@@ -332,7 +332,7 @@ class TestSwifter(unittest.TestCase):
 
     def test_vectorized_math_apply_on_large_rolling_dataframe(self):
         df = pd.DataFrame(
-            {"x": np.arange(0, 1_000_000)}, index=pd.date_range("2019-01-1", "2020-01-1", periods=1_000_000)
+            {"x": np.arange(0, 1_500_000)}, index=pd.date_range("2019-01-1", "2020-01-1", periods=1_500_000)
         )
 
         start_pd = time.time()
