2b241af5925e63634a5a257b376f533cdabb4b7b
jmcarpenter2 <jcarpenter@manifold.ai>
2020-07-26 20:56:05 -0700

    fix imports

--- swifter/__init__.py
+++ swifter/__init__.py
@@ -1,2 +1,12 @@
 # flake8: noqa
+
+from .swifter import SeriesAccessor, DataFrameAccessor
+from .parallel_accessor import register_parallel_dataframe_accessor, register_parallel_series_accessor
+
+__all__ = [
+    "SeriesAccessor",
+    "DataFrameAccessor",
+    "register_parallel_dataframe_accessor",
+    "register_parallel_series_accessor",
+]
 __version__ = "0.400"
