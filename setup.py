4b73c8fcfaf53edba86345816e6523c40524312f
jmcarpenter2 <jcarpenter@manifold.ai>
2020-08-03 17:34:43 -0700

    Update dependency versions

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     url="https://github.com/jmcarpenter2/swifter",  # use the URL to the github repo
     download_url="https://github.com/jmcarpenter2/swifter/archive/1.0.3.tar.gz",
     keywords=["pandas", "dask", "apply", "function", "parallelize", "vectorize"],
-    install_requires=["pandas>=0.23.0", "psutil>=5.6.6", "dask[complete]>=0.19.0", "tqdm>=4.33.0", "ipywidgets>=7.0.0", "parso>0.4.0", "bleach>=3.1.1", "modin[ray]>=0.7.4"],
+    install_requires=["pandas>=1.0.0", "psutil>=5.6.6", "dask[dataframe]>=2.10.0", "tqdm>=4.33.0", "ipywidgets>=7.0.0", "parso>0.4.0", "bleach>=3.1.1", "modin[ray]>=0.7.4"],
     classifiers=[],
 )
