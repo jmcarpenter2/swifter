595b16ed1f6e02609b5f25e9008501110546f57f
Austin Fox <openafox@gmail.com>
2021-06-23 16:52:51 -0700

    Make modin optional and allow user to decide which modin version to use
    #150 #147 
    Allows users to install like pip install swifter, swifter[ray], or swifter[dask] for no modin, modin[ray] and modin[dask] respectively.
    See setuptools docs here:  https://setuptools.readthedocs.io/en/latest/userguide/dependency_management.html#optional-dependencies

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ setup(
     url="https://github.com/jmcarpenter2/swifter",  # use the URL to the github repo
     download_url="https://github.com/jmcarpenter2/swifter/archive/1.0.7.tar.gz",
     keywords=["pandas", "dask", "apply", "function", "parallelize", "vectorize"],
-    install_requires=["pandas>=1.0.0", "psutil>=5.6.6", "dask[dataframe]>=2.10.0", "tqdm>=4.33.0", "ipywidgets>=7.0.0" "cloudpickle>=0.2.2", "parso>0.4.0", "bleach>=3.1.1", "modin[ray]>=0.8.1.1"],
+    install_requires=["pandas>=1.0.0", "psutil>=5.6.6", "dask[dataframe]>=2.10.0", "tqdm>=4.33.0", "ipywidgets>=7.0.0" "cloudpickle>=0.2.2", "parso>0.4.0", "bleach>=3.1.1"],
+    extras_require={
+        "ray": ["modin[ray]>=0.8.1.1"],
+        "dask": ["modin[dask]>=0.8.1.1"]
+    },
     classifiers=[],
 )
