5b3facb5460f7772a13982c4de7709d1c21ab01b
Jason Carpenter <Jason.Carpenter@resmed.com>
2022-02-04 17:12:54 -0800

    Release 1.1.0

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup, Extension
+from swifter.__init__ import __version__
 
 with open("requirements.txt", "rb") as f:
     requirements = [req.strip() for req in f.readlines()]
@@ -6,12 +7,12 @@ with open("requirements.txt", "rb") as f:
 setup(
     name="swifter",
     packages=["swifter"],  # this must be the same as the name above
-    version="1.0.9",
+    version=__version__,
     description="A package which efficiently applies any function to a pandas dataframe or series in the fastest available manner",
     author="Jason Carpenter",
     author_email="jcarpenter@manifold.ai",
     url="https://github.com/jmcarpenter2/swifter",  # use the URL to the github repo
-    download_url="https://github.com/jmcarpenter2/swifter/archive/1.0.9.tar.gz",
+    download_url=f"https://github.com/jmcarpenter2/swifter/archive/{__version__}.tar.gz",
     keywords=["pandas", "dask", "apply", "function", "parallelize", "vectorize"],
     install_requires=requirements,
     classifiers=[],
