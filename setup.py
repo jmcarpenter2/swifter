bc754088feb97f883794585a7eeb8cb5914e1d4b
Jason Carpenter <Jason.Carpenter@resmed.com>
2022-02-07 09:34:52 -0800

    Fix refs

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Jason Carpenter",
     author_email="jcarpenter@manifold.ai",
     url="https://github.com/jmcarpenter2/swifter",  # use the URL to the github repo
-    download_url=f"https://github.com/jmcarpenter2/swifter/archive/{VERSION}.tar.gz",
+    download_url=f"https://github.com/jmcarpenter2/swifter/archive/1.1.2.tar.gz",
     keywords=["pandas", "dask", "apply", "function", "parallelize", "vectorize"],
     install_requires=[
         "pandas>=1.0.0",
